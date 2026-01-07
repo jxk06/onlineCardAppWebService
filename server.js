4751d8a9a3a487d0c15671dcc6d5a87db1e7df23
Koh Jing Xi <24011503@myrp.edu.sg>
2026-01-07 15:43:51 +0800

    test

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = 3000;
 
 
 const dbConfig = {
-    host: 'process.env.DB_HOST',
-    user: 'process.env.DB_USER',
-    password: 'process.env.DB_PASSWORD',
-    database: 'process.env.DB_NAME',
-    port: 'process.env.PORT',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.PORT,
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
